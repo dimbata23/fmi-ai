b5462c86cde64b7bfcc624b4002b35739ee8fc63
dimbata23 <adimitrov23@gmail.com>
2022-04-04 15:59:21 +0300

    mutations

--- hw1-tsp/tsp-ai/src/main.rs
+++ hw1-tsp/tsp-ai/src/main.rs
@@ -7,31 +7,44 @@ fn main() {
     println!("{:?} with road len: {}", res_ind, fitness( &res_ind ));
 }
 
-lazy_static! {
-    static ref GRAPH: HashMap<char, HashMap<char, u32>> = HashMap::from([
-        ('A', HashMap::from([('A', 00), ('B', 36), ('C', 32), ('D', 54), ('E', 20), ('F', 40)])),
-        ('B', HashMap::from([('A', 36), ('B', 00), ('C', 22), ('D', 58), ('E', 54), ('F', 67)])),
-        ('C', HashMap::from([('A', 32), ('B', 22), ('C', 00), ('D', 36), ('E', 42), ('F', 71)])),
-        ('D', HashMap::from([('A', 54), ('B', 58), ('C', 36), ('D', 00), ('E', 50), ('F', 92)])),
-        ('E', HashMap::from([('A', 20), ('B', 54), ('C', 42), ('D', 50), ('E', 00), ('F', 45)])),
-        ('F', HashMap::from([('A', 40), ('B', 67), ('C', 71), ('D', 92), ('E', 45), ('F', 00)])),
-    ]);
-}
-
-const ISIZE: usize = 6;
-type Individual = [char; ISIZE];
-
-fn fitness(ind: &Individual) -> u32 {
-    let mut res = 0;
-    for i in 0..(ISIZE - 1) {
-        let curr    = &ind[i];
-        let next    = &ind[i + 1];
-        res += GRAPH[ curr ][ next ];
+fn genetic_algorithm(init_size: usize, fitness_func: fn(&Individual) -> u32) -> Option<Individual> {
+    let     range       = ['A', 'B', 'C', 'D', 'E', 'F'];
+    let mut rng         = rand::thread_rng();
+    let mut population  = BTreeSet::new();
+    for _ in 0..init_size {
+        let mut ind     = range.clone();
+        ind.shuffle( &mut rng );
+        population.insert( ind );
     }
 
-    let last    = ind.last().unwrap();
-    let first   = ind.first().unwrap();
-    res + GRAPH[ last ][ first ]
+    // TODO: replace with real end condition
+    for _ in 0..100 {
+        let mut new_population  = BTreeSet::new();
+
+        for _ in &population {
+            let par_i   = rng.gen_range( 0..population.len() );
+            let par1    = population.iter().skip( par_i ).next().unwrap();
+            let par_j   = rng.gen_range( 0..population.len() );
+            let par2    = population.iter().skip( par_j ).next().unwrap();
+
+            let (mut child1, mut child2)    = reproduce( &par1, &par2 );
+            if rng.gen_bool( 0.05 ) {
+                child1  = mutate( &child1 );
+            }
+
+            if rng.gen_bool( 0.05 ) {
+                child2  = mutate( &child2 );
+            }
+            new_population.insert( child1 );
+            new_population.insert( child2 );
+        }
+
+        population  = new_population;
+    }
+
+    println!("{:?}", population.iter().take( 10 ).map( fitness_func ).collect::<Vec<_>>() );
+
+    population.into_iter().min_by( |i, j| fitness_func( i ).cmp( &fitness_func( j ) ) )
 }
 
 fn reproduce(lhs: &Individual, rhs: &Individual) -> (Individual, Individual) {
@@ -53,45 +66,57 @@ fn reproduce(lhs: &Individual, rhs: &Individual) -> (Individual, Individual) {
     (make_unique( lres ), make_unique( rres ))
 }
 
-fn genetic_algorithm(init_size: usize, fitness_func: fn(&Individual) -> u32) -> Option<Individual> {
-    let     range       = ['A', 'B', 'C', 'D', 'E', 'F'];
+fn mutate(ind: &Individual) -> Individual {
     let mut rng         = rand::thread_rng();
-    let mut population  = BTreeSet::new();
-    for _ in 0..init_size {
-        let mut ind     = range.clone();
-        ind.shuffle( &mut rng );
-        population.insert( ind );
-    }
+    let mut pos_i   = rng.gen_range( 0..ISIZE );
+    let mut pos_j   = rng.gen_range( 0..ISIZE );
 
-    // TODO: replace with real end condition
-    for _ in 0..100 {
-        let mut new_population  = BTreeSet::new();
-
-        for _ in &population {
-            let par_i   = rng.gen_range( 0..population.len() );
-            let par1    = population.iter().skip( par_i ).next().unwrap();
-            let par_j   = rng.gen_range( 0..population.len() );
-            let par2    = population.iter().skip( par_j ).next().unwrap();
-
-            let (child1, child2)    = reproduce( &par1, &par2 );
-            // if rng.gen_bool( 0.01 ) {
-            //     child1  = mutate( &child1 );
-            // }
-
-            // if rng.gen_bool( 0.01 ) {
-            //     child2  = mutate( &child2 );
-            // }
-            new_population.insert( child1 );
-            new_population.insert( child2 );
+    if pos_i == pos_j {
+        if pos_i > 0 {
+            pos_i -= 1;
+        } else {
+            pos_j = 1;
         }
-
-        population  = new_population;
     }
 
-    println!("{:?}", population.iter().take( 10 ).map( fitness_func ).collect::<Vec<_>>() );
+    if pos_i > pos_j {
+        std::mem::swap( &mut pos_i, &mut pos_j );
+    }
 
-    population.into_iter().min_by( |i, j| fitness_func( i ).cmp( &fitness_func( j ) ) )
+    let mut res_ind = ind.clone();
+    let (lhs, rhs)  = res_ind.split_at_mut( pos_j );
+    
+    std::mem::swap( &mut lhs[ pos_i ], &mut rhs[ 0 ] );
+
+    res_ind
 }
+
+fn fitness(ind: &Individual) -> u32 {
+    let mut res = 0;
+    for i in 0..(ISIZE - 1) {
+        let curr    = &ind[i];
+        let next    = &ind[i + 1];
+        res += GRAPH[ curr ][ next ];
+    }
+
+    let last    = ind.last().unwrap();
+    let first   = ind.first().unwrap();
+    res + GRAPH[ last ][ first ]
+}
+
+lazy_static! {
+    static ref GRAPH: HashMap<char, HashMap<char, u32>> = HashMap::from([
+        ('A', HashMap::from([('A', 00), ('B', 36), ('C', 32), ('D', 54), ('E', 20), ('F', 40)])),
+        ('B', HashMap::from([('A', 36), ('B', 00), ('C', 22), ('D', 58), ('E', 54), ('F', 67)])),
+        ('C', HashMap::from([('A', 32), ('B', 22), ('C', 00), ('D', 36), ('E', 42), ('F', 71)])),
+        ('D', HashMap::from([('A', 54), ('B', 58), ('C', 36), ('D', 00), ('E', 50), ('F', 92)])),
+        ('E', HashMap::from([('A', 20), ('B', 54), ('C', 42), ('D', 50), ('E', 00), ('F', 45)])),
+        ('F', HashMap::from([('A', 40), ('B', 67), ('C', 71), ('D', 92), ('E', 45), ('F', 00)])),
+    ]);
+}
+
+const ISIZE: usize = 6;
+type Individual = [char; ISIZE];
 
 
 
